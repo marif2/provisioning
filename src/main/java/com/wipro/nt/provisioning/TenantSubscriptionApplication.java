20aa96b051ca86e884d7867c571cacb63cc39f9d
Mohammed Arif <m_arif1@yahoo.com>
2016-07-11 17:42:27 +0530

    Update TenantSubscriptionApplication.java

--- src/main/java/com/wipro/nt/provisioning/TenantSubscriptionApplication.java
+++ src/main/java/com/wipro/nt/provisioning/TenantSubscriptionApplication.java
@@ -3,16 +3,19 @@ package com.wipro.nt.provisioning;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/subscribe")
-public class TenantSubscriptionApplication {
+public class TenantSubscriptionApplication extends SpringBootServletInitializer{
 	
 	@Autowired
 	JenkinsConfig jenkinsConfig;
@@ -34,6 +37,12 @@ public class TenantSubscriptionApplication {
         return "{\"subscribe\":\"successful\",\"result\":\""+result+"\"}";
     }
    
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TenantSubscriptionApplication.class);
+	}
+
+	
     public static void main(String[] args) {
         SpringApplication.run(TenantSubscriptionApplication.class, args);
     }
